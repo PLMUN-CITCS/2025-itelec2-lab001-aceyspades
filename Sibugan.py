5f08bf2f830123895d67b5bec3f364c314c1bb42
aceyspades <sibuganalwinace_bscrim@plmun.edu.ph>
2025-02-10 15:25:02 +0800

    Create Sibugan.py

---
+++ Sibugan.py
@@ -0,0 +1,10 @@
+print("Hello world!")
+
+print("Hello world!")
+print("Name: [Alwin Ace Sibugan]")
+print("Course: [BSIT]")
+print("Interests in programming: [I have no interest]")
+
+Name: [Alwin Ace Sibugan]
+Course: [BSIT]
+Interests in programming: [I have no interest]
